29c1ec4931ab3d1a0a8290e1a5f994bd5079f466
hemant <srivastavhemant17@gmail.com>
2023-10-02 19:59:55 +0530

    fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.scss';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Home from './COMPONENTS/Home/Home.jsx';
+import Header from './COMPONENTS/Header/Header';
 
 const App = () => {
   return (
     <>
+    <Header/>
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
--- src/COMPONENTS/Header/Header.jsx
+++ src/COMPONENTS/Header/Header.jsx
@@ -1,11 +1,26 @@
-
+// import { Link } from "react-router-dom";
+import { ImSearch } from "react-icons/im"; // Correct the import statement for ImSearch
+import NetflixLogo from "../../images/Netflix.png"; // Correct the path to the image
+import "./Header.scss";
 
 const Header = () => {
   return (
-    <>
-      <nav className="header"></nav>
-    </>
-  )
-}
+    <nav className="Header">
+      {/* Move the text inside a <h1> tag for semantic purposes */}
+   
+      <img src={NetflixLogo} alt="Netflix Logo" /> {/* Use a more descriptive alt text */}
+      <div>
+        
+        <a href="/tvshows">TV Shows</a>
+        <a href="/movies">Movies</a>
+        <a href="/recent">Recently Added</a>
+        <a href="/mylist">My List</a>
+       
+        {/* Add more navigation links here if needed */}
+      </div>
+      <ImSearch />
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
